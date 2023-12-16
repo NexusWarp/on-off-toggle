5f36ff9ecb287ebef86dd9b1c8d06de687a6135a
NexusWarp <ik571572@gmail.com>
2023-12-16 15:48:01 +0500

    second update

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-$(".outline").click(function(){
-    $(".btn").toggleClass("anim");
-    $(".outer").toggleClass("change");
-});
+$(document).ready(function() {
+   
+    function getRandomColor() {
+      return '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
+
+
+    function addRandomColor() {
+      var randomColor = getRandomColor();
+      $('.outer').css('background-color', randomColor);
+    }
+
+  
+    $('.outline').on('click', function() {
+      addRandomColor();
+    });
+  });
+
